00ceefe4e20180237b6161ee9d5aead1e0bbe1c2
Alis-1 <tatu.tammi@edu.turkuamk.fi>
2025-01-18 16:27:07 +0200

    Exercises 2.1.-2.5.

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,58 +1,58 @@
+import Course from './Course';
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
-
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map((part, index) => (
-        <Part key={index} part={part} />
+      {courses.map(course => (
+        <Course key={course.id} course={course} />
       ))}
-    </>
-  )
-}
+    </div>
+  );
+};
 
-const Part = ({ part }) => {
-  return (
-    <p>{part.name} {part.exercises}</p>
-  )
-}
-
-const Total = ({ parts }) => {
-  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0)
-  return (
-    <p>Yhteensä {totalExercises} tehtävää</p>
-  )
-}
-
-export default App
+export default App;
