2342e7e4d340181b1d8f781706342600aff011bb
Alis-1 <tatu.tammi@edu.turkuamk.fi>
2025-01-18 16:50:45 +0200

    Exercises 2.6.-2.10.

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      const personObject = {
+        name: newName,
+        number: newNumber
+      };
+      setPersons(persons.concat(personObject));
+      setNewName('');
+      setNewNumber('');
+    }
+  };
+
+  const personsToShow = persons.filter(person => 
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={handleFilterChange} />
+      </div>
+      <form onSubmit={addPerson}>
+        <h2>Add a new</h2>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {personsToShow.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
