121bbef2a5b37abf78632fca7b5bbf43ba0f2410
Alis-1 <tatu.tammi@edu.turkuamk.fi>
2025-03-08 15:54:10 +0200

    2.11

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,15 +1,31 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
+  const [notes, setNotes] = useState([]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled');
+        setNotes(response.data);
+      });
+
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('persons fetched');
+        setPersons(response.data);
+      });
+  }, []);
+  console.log('render', notes.length, 'notes');
+  console.log('render', persons.length, 'persons');
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
