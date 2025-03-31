860511c369eb3b41a90881b7520df2725b267811
Alis-1 <tatu.tammi@edu.turkuamk.fi>
2025-03-31 16:54:19 +0300

    Exercise 3.12

---
+++ osa3/puhelinluettelo/backend/mongo.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Anna salasana')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+  // Näytä kaikki numerot
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+} else if (process.argv.length === 5) {
+  // Lisää uusi numero
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({
+    name: name,
+    number: number,
+  })
+
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  console.log('Virheellinen määrä parametreja')
+  mongoose.connection.close()
+} 
