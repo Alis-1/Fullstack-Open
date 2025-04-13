c1b681d665bde50ce24265708978eada63c5493f
Alis-1 <tatu.tammi@edu.turkuamk.fi>
2025-04-13 15:25:20 +0300

    Exercise 9.12.-9.13.

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import { v1 as uuid } from 'uuid';
 import diagnoses from './data/diagnoses';
 import patients from './data/patients';
-import { NonSensitivePatient } from './types';
+import { NonSensitivePatient, NewPatient, Gender } from './types';
 
 const app = express();
 app.use(cors());
@@ -27,6 +28,38 @@ app.get('/api/patients', (_req, res) => {
     occupation
   }));
   res.send(nonSensitivePatients);
+});
+
+app.post('/api/patients', (req, res) => {
+  try {
+    const { name, dateOfBirth, ssn, gender, occupation } = req.body as NewPatient;
+
+    if (!name || !dateOfBirth || !ssn || !gender || !occupation) {
+      return res.status(400).json({ error: 'parameters missing' });
+    }
+
+    if (!Object.values(Gender).includes(gender)) {
+      return res.status(400).json({ error: 'invalid gender' });
+    }
+
+    const newPatient: NewPatient = {
+      name,
+      dateOfBirth,
+      ssn,
+      gender,
+      occupation
+    };
+
+    const addedPatient = {
+      ...newPatient,
+      id: uuid()
+    };
+
+    patients.push(addedPatient);
+    res.json(addedPatient);
+  } catch (error) {
+    res.status(400).json({ error: 'malformatted parameters' });
+  }
 });
 
 app.listen(PORT, () => {
