acd50f83a75917ab043ace9e9f77f2a5022e6936
Alis-1 <tatu.tammi@edu.turkuamk.fi>
2025-04-13 15:23:35 +0300

    Exercise 9.10.-9.11.

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import diagnoses from './data/diagnoses';
+import patients from './data/patients';
+import { NonSensitivePatient } from './types';
 
 const app = express();
 app.use(cors());
@@ -9,6 +12,21 @@ const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
+});
+
+app.get('/api/diagnoses', (_req, res) => {
+  res.send(diagnoses);
+});
+
+app.get('/api/patients', (_req, res) => {
+  const nonSensitivePatients: NonSensitivePatient[] = patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation
+  }));
+  res.send(nonSensitivePatients);
 });
 
 app.listen(PORT, () => {
